04db90316d709d545f4a0bd8e261d24711f642a3
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2023-12-28 19:55:06 +0200

    2

---
+++ src/components/Feedback/LeaveFeedback.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class LeaveFeedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return <div>Class Component</div>;
+  }
+}
+
+export default LeaveFeedback;
